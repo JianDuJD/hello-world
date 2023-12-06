10b462a146f17395490370ef7d54894a932c2d3c
dujian <1104453477@qq.com>
2023-12-06 20:48:08 +0800

    道法自然

---
+++ data-structures-and-algorithms/src/main/java/queue/Deque.java
@@ -0,0 +1,26 @@
+package queue;
+
+import linkedList.LinkedList;
+import linkedList.Node;
+
+public class Deque<E> {
+
+    private LinkedList<E> linkedList;
+    private Node<E> firstNode;
+    private Node<E> lastNode;
+    private int size;
+
+    public void pushLast(E element) {
+
+        linkedList.
+    }
+
+    public E popLast() {
+    }
+
+    public void pushFirst() {
+    }
+
+    public E popFirst() {
+    }
+}
