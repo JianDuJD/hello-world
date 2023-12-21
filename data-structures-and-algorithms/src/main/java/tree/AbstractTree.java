c125747862c5103eae2e2030074b36abff746349
dujian <1104453477@qq.com>
2023-12-22 02:10:25 +0800

    道法自然

--- data-structures-and-algorithms/src/main/java/tree/AbstractTree.java
+++ data-structures-and-algorithms/src/main/java/tree/AbstractTree.java
@@ -4,25 +4,4 @@ import linkedList.LinkedList;
 
 public abstract class AbstractTree<E> {
 
-    protected TreeNode<E> root;
-    private int height;
-
-    private int degree;
-
-    public AbstractTree(int degree) {
-        root = new TreeNode<E>();
-        this.degree = degree;
-    }
-
-    public abstract void add(TreeNode<E> node);
-
-    public abstract void remove(TreeNode<E> node);
-
-    public int getDegree() {
-        return degree;
-    }
-
-    public void setDegree(int degree) {
-        this.degree = degree;
-    }
 }
--- data-structures-and-algorithms/src/main/java/tree/BinaryTree.java
+++ data-structures-and-algorithms/src/main/java/tree/BinaryTree.java
@@ -5,37 +5,30 @@ import java.util.TreeSet;
 
 public class BinaryTree<E> extends AbstractTree<E>{
 
+    private int[] val = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
-    private TreeNode<E> lastLeftNode;
 
-    private TreeNode<E> lastRightNode;
-
-    public BinaryTree() {
-        super(2);
+    public void print(int i){
+        int index = 0;
+        for (int i1 = 1; i1 < i+1; i1++) {
+            int i2 = tierNodeNum(i1);
+            for (int i3 = 0; i3 < i2; i3++) {
+                System.out.print(val[index]+",");
+                index++;
+            }
+            System.out.println("");
+        }
     }
 
-    @Override
-    public void add(TreeNode<E> node) {
-
-    }
-
-    @Override
-    public void remove(TreeNode<E> node) {
-
-    }
-
-    public void addOnLeft(TreeNode<E> node){
-        lastLeftNode.setChild();
-    }
-    public TreeNode<E> getRootNode() {
-        return rootNode;
-    }
-
-    public TreeNode<E> getLastLeftNode() {
-        return lastLeftNode;
-    }
-
-    public TreeNode<E> getLastRightNode() {
-        return lastRightNode;
+    public int tierNodeNum(int i){
+        if (i == 1){
+            return 1;
+        }
+        int num = i-1;
+        int a = 1;
+        for (int i1 = 0; i1 < num; i1++) {
+            a = a*2;
+        }
+        return a;
     }
 }
