037cf6e0295710522e2d96d5c1ad2606614f6708
dujian <1104453477@qq.com>
2023-12-22 02:10:39 +0800

    道法自然

---
+++ data-structures-and-algorithms/src/main/java/tree/Test.java
@@ -0,0 +1,8 @@
+package tree;
+
+public class Test {
+    public static void main(String[] args) {
+        BinaryTree<Integer> integerBinaryTree = new BinaryTree<>();
+        integerBinaryTree.print(4);
+    }
+}
